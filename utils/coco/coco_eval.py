01fa44f7d4eba84dbe184b09ff6fc155690a0b7e
Sergio Hinojosa <sergio2pi@gmail.com>
2023-12-08 21:19:57 -0300

    torch._six deprecated

--- utils/coco/coco_eval.py
+++ utils/coco/coco_eval.py
@@ -5,7 +5,6 @@ from collections import defaultdict
 import numpy as np
 import pycocotools.mask as mask_util
 import torch
-import torch._six
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
